5eca2096f08f9b3c0c914bd0f10ff91d8b27a2a6
Emmanuelle Bourget <emmanuelle.bourget@gmail.com>
2025-02-12 14:50:03 +0000

    finance days model

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,22 @@
+-- Date
+-- Total number of transactions
+-- Total revenue
+-- Avereage Basket
+-- Operational Margin
+-- Total purchase cost
+-- Total shipping fees
+-- Total log costs
+-- Total quantity of products sold.
+
+SELECT
+date_date
+,COUNT(op_marg.orders_id) AS nb_transactions
+,ROUND(SUM(op_marg.revenue),2) AS revenue
+,ROUND(SUM(op_marg.revenue)/SUM(op_marg.quantity),2) AS avereage_basket
+,SUM(op_marg.purchase_cost) AS purchase_cost
+,SUM(op_marg.shipping_fees) AS shipping_fees
+,SUM(op_marg.log_cost) AS log_cost
+,SUM(op_marg.quantity) AS quantity
+FROM {{ref("int_orders_operational")}} AS op_marg
+GROUP by date_date 
+order by date_date DESC
